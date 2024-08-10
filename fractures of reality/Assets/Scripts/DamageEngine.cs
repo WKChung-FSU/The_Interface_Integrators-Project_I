faec2f83e0300f80f38bba5689d68cde0c29346c
William chung <155984229+WKChung-FSU@users.noreply.github.com>
2024-08-09 22:33:02 -0400

    added DamageEngine
    made a basic script for damage, basic spell works , no damage testing yet, the engine has room for more spells if needed

---
+++ fractures of reality/Assets/Scripts/DamageEngine.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DamageEngine : MonoBehaviour
+{
+    // will add more spell types if necessary
+    public enum damageType { spellBasic, Environmental }
+    [SerializeField] damageType type;
+    [SerializeField] Rigidbody rb; 
+
+    [SerializeField] int damageAmount;
+    [SerializeField] int speed;
+    [SerializeField] int RemoveTime;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //logic for the spell bullet itself
+        if (type == damageType.spellBasic)
+        {
+            rb.velocity = transform.forward * speed;
+            Destroy(gameObject, RemoveTime);
+        }
+
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.isTrigger)
+        {
+            return;
+        }
+        IDamage dmg = other.GetComponent<IDamage>();
+        if (dmg != null)
+        {
+            dmg.takeDamage(damageAmount,type);
+        }
+        Destroy(gameObject);
+    }
+}
--- fractures of reality/Assets/Scripts/IDamage.cs
+++ fractures of reality/Assets/Scripts/IDamage.cs
@@ -1,11 +1,14 @@
-    using System.Collections;
+using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public interface IDamage
 {
-    void takeDamage(int amount)
+    //causes damage to object or player, will default to basic magic bullet
+    void takeDamage(int amount,DamageEngine.damageType DamageType=0)
     {
 
     }
+
 }
