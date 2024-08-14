f1cab84551c69cea65a1e90c1ca7cd07ec847f58
Christopher Mathews <cmathews58388@gmail.com>
2024-08-13 19:57:15 -0700

    Many UI changes
    Ammo + heath bars now update
    other stuff

--- fractures of reality/Assets/Scripts/gameManager.cs
+++ fractures of reality/Assets/Scripts/gameManager.cs
@@ -5,6 +5,7 @@ using TMPro;
 //using UnityEditor.SearchService;
 using Unity.VisualScripting;
 using UnityEditor;
+using UnityEngine.UI;
 
 public class gameManager : MonoBehaviour
 {
@@ -14,6 +15,11 @@ public class gameManager : MonoBehaviour
     public GameObject player;
     public playerControler playerScript;
     public PlayerWeapon playerWeapon;
+    int enemyCount;
+    int healthCount;
+    public int healthMax;
+    int ammoCount;
+    public int ammoMax;
     #endregion
 
     #region UI
@@ -22,8 +28,8 @@ public class gameManager : MonoBehaviour
     [SerializeField] GameObject menuWin;
     [SerializeField] GameObject menuLose;
     [SerializeField] GameObject menuHUD;
-    [SerializeField] GameObject healthBar;
-    [SerializeField] GameObject ammoBar;
+    [SerializeField] Image healthBar;
+    [SerializeField] Image ammoBar;
     [SerializeField] GameObject damageFlashScreen;
     [SerializeField] TMP_Text healthCountText;
     [SerializeField] TMP_Text ammoCountText;
@@ -32,9 +38,6 @@ public class gameManager : MonoBehaviour
     public bool hudEnabled;
     #endregion
 
-    int enemyCount;
-    int healthCount;
-    int ammoCount;
 
     // Start is called before the first frame update
     void Awake()
@@ -135,12 +138,14 @@ public class gameManager : MonoBehaviour
     {
         healthCount = playerScript.PlayerHP;
         healthCountText.text = healthCount.ToString("F0");
+        gameManager.instance.healthBar.fillAmount = (float)healthCount / healthMax;
     }
 
     void UpdateAmmoBar()
     {
         ammoCount = playerWeapon.GetCurrentAmmo();
         ammoCountText.text = ammoCount.ToString("F0");
+        gameManager.instance.ammoBar.fillAmount = (float)ammoCount / ammoMax;
     }
 
     IEnumerator DamageFlashTimer()
