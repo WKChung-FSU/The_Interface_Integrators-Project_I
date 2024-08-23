afd2ea29e7e84ca4e941f237e223f544b922d9dd
Joseph Paine <japaine02@gmail.com>
2024-08-23 16:59:05 -0500

    Setting up Win platforms as well as trigger scripts

---
+++ fractures of reality/Assets/Scripts/Trigger.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Trigger : MonoBehaviour
+{
+    [SerializeField] UnityEvent onTrigEnter;
+    [SerializeField] UnityEvent onTrigExit;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        onTrigEnter.Invoke();
+
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        onTrigExit.Invoke();
+    }
+}
