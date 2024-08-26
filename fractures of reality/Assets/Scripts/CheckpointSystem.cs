a6c526d462e286b626c8a8bcf65cec192ba480c7
William chung <155984229+WKChung-FSU@users.noreply.github.com>
2024-08-25 20:57:57 -0400

    updated checkpoint system added a waypoint and fixed the ice spear

--- fractures of reality/Assets/Scripts/CheckpointSystem.cs
+++ fractures of reality/Assets/Scripts/CheckpointSystem.cs
@@ -1,4 +1,3 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -7,39 +6,60 @@ public class CheckpointSystem : MonoBehaviour
     [Header("----- Main Attributes -----")]
     Color colorOriginal;
     [SerializeField] Renderer model;
+    [SerializeField] List<GameObject> WaypointParticles = new List<GameObject>();
+      [Header("----- Special Attributes -----")]
     [SerializeField] DamageEngine.ElementType NewElement;
-    [Header("----- Special Attributes -----")]
-
     [SerializeField] bool IsCheckpoint;
     [SerializeField] bool IsElementPoint;
-
+    [SerializeField] bool isTeleporter;
+    [SerializeField] GameObject TLGameObject;
+    [SerializeField] Vector3 TransportPosition;
     void Start()
     {
+        if (TLGameObject != null)
+        {
+            TransportPosition = TLGameObject.transform.position;
+        }
+        ///COCONUT.JPG IS REAL!!!!!!!!!!!!
+        ToggleParticles(false);
+
+        if (IsCheckpoint) 
+        isTeleporter=false;
+
         colorOriginal = model.material.color;
-    }
+    }   
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.isTrigger)
             return;
-        if ((other.CompareTag("Player") && gameManager.instance.StartPosition() != this.transform.position)&&IsCheckpoint)
+        if ((other.CompareTag("Player") && gameManager.instance.StartPosition() != this.transform.position)&&IsCheckpoint||isTeleporter)
         {
-            gameManager.instance.StartPosition(transform.position);
-            StartCoroutine(FlashModel());
+            if (isTeleporter)
+            {
+                gameManager.instance.StartPosition(TransportPosition);
+                // sent player to start position
+            }
+            else
+            {
+                gameManager.instance.StartPosition(transform.position);
+            }
+            ToggleParticles(true);
+            // disable previous waypoint particle
         }
         DestructibleHealthCore otherHealth = other.GetComponent<DestructibleHealthCore>();
         if (IsElementPoint && otherHealth != null)
         {
             otherHealth.ElementType= NewElement;
+            ToggleParticles(true);
         }
 
     }
-    IEnumerator FlashModel()
+    void ToggleParticles(bool Particle)
     {
-       
-        model.material.color = Color.red;
-        yield return new WaitForSeconds(0.3f);
-        model.material.color = colorOriginal;
-       
+       foreach (GameObject Object in WaypointParticles)
+        {
+            Object.SetActive(Particle);
+        }
     }
 }
