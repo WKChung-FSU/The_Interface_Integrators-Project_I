39d0136af1a43daff47e88fe9f7bfd181e9b3084
GryphenS117 <148662726+GryphenS117@users.noreply.github.com>
2024-08-27 23:51:27 -0400

    pickup sounds
    added sounds for health and ammo pickup

--- fractures of reality/Assets/Scripts/pickup.cs
+++ fractures of reality/Assets/Scripts/pickup.cs
@@ -4,7 +4,13 @@ using UnityEngine;
 
 public class pickup : MonoBehaviour
 {
-   
+    [Header("----- Sounds -----")]
+    [SerializeField] AudioClip[] AudioHealth;
+    [Range(0, 1)][SerializeField] float AudioHealthVol = 0.5f;
+
+    [SerializeField] AudioClip[] AudioAmmo;
+    [Range(0, 1)][SerializeField] float AudioAmmoVol = 0.5f;
+
     public int healthBonus =25;
     public int AmmoBonus = 10;
     
@@ -20,6 +26,7 @@ public class pickup : MonoBehaviour
             {
                 if (other.GetComponent<DestructibleHealthCore>().HP < other.GetComponent<DestructibleHealthCore>().HPMax)
                 {
+                    gameManager.instance.playAudio(AudioHealth[Random.Range(0, AudioHealth.Length)], AudioHealthVol);
                     Destroy(gameObject);
                     other.GetComponent<DestructibleHealthCore>().HP += healthBonus;
                     if (other.GetComponent<DestructibleHealthCore>().HP > other.GetComponent<DestructibleHealthCore>().HPMax)
@@ -36,6 +43,7 @@ public class pickup : MonoBehaviour
             {
                 if (other.GetComponent<PlayerWeapon>().Ammo < other.GetComponent<PlayerWeapon>().maxAmmo)
                 {
+                    gameManager.instance.playAudio(AudioAmmo[Random.Range(0, AudioAmmo.Length)], AudioAmmoVol);
                     Destroy(gameObject);
                     other.GetComponent<PlayerWeapon>().Ammo += AmmoBonus;
                     if (other.GetComponent<PlayerWeapon>().Ammo > other.GetComponent<PlayerWeapon>().maxAmmo)
