e4a086ad73886d949e17901642ee5f95f90db24f
GryphenS117 <148662726+GryphenS117@users.noreply.github.com>
2024-09-13 02:29:14 -0400

    Main Loading Screen
    Made Initial Loading Screen

--- fractures of reality/Assets/MainMenu.cs
+++ fractures of reality/Assets/MainMenu.cs
@@ -2,12 +2,28 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour
 {
-   public void PlayGame()
+    public GameObject LoadingScreen;
+    public Slider slider;
+   public void PlayGame( int sceneIndex)
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        StartCoroutine(LoadAsync(sceneIndex));
+    }
+    IEnumerator LoadAsync(int sceneIndex)
+    {
+        AsyncOperation operation = SceneManager.LoadSceneAsync(sceneIndex);
+
+        LoadingScreen.SetActive(true);
+
+        while (!operation.isDone)
+        {
+            float progress = Mathf.Clamp01(operation.progress / .9f);
+            slider.value = progress;
+            yield return null;
+        }
     }
     public void QuitGame()
     {
