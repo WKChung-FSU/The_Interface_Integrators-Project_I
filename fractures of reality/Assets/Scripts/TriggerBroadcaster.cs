3e0afb7594db75ef71b1abeeb58a563a134eb3ad
Christopher Mathews <cmathews58388@gmail.com>
2024-09-20 23:42:12 -0700

    Boss level should now be completely finished.
    That moment when you go to pull before you push and somehow undo all of your work :(

--- fractures of reality/Assets/Scripts/TriggerBroadcaster.cs
+++ fractures of reality/Assets/Scripts/TriggerBroadcaster.cs
@@ -5,12 +5,13 @@ using UnityEngine;
 public class TriggerBroadcaster : MonoBehaviour
 {
     [SerializeField] GameObject listener;
+    [SerializeField] bool setListenerActive = true;
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
-            listener.SetActive(true);
+            listener.SetActive(setListenerActive);
 
             Destroy(this.gameObject);
         }
